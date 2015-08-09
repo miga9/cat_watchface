84d0adb92a4f82fa0c6b38466febee1a5b332345
miga <miguel.g.kano@gmail.com>
2015-08-09 00:33:59 -0500

    Update the displayed time every minute

--- wear/src/main/java/com/migapro/catwatchface/CatWatchFaceService.java
+++ wear/src/main/java/com/migapro/catwatchface/CatWatchFaceService.java
@@ -3,10 +3,16 @@ package com.migapro.catwatchface;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.wearable.watchface.CanvasWatchFaceService;
 import android.view.SurfaceHolder;
 
+import java.util.concurrent.TimeUnit;
+
 public class CatWatchFaceService extends CanvasWatchFaceService {
+
+    private static final long TICK_PERIOD = TimeUnit.MINUTES.toMillis(1);
 
     @Override
     public Engine onCreateEngine() {
@@ -15,7 +21,25 @@ public class CatWatchFaceService extends CanvasWatchFaceService {
 
     private class Engine extends CanvasWatchFaceService.Engine {
 
+        private static final int MSG_UPDATE_TIME = 0;
+
         private CatWatchFace mWatchFace;
+        private final Handler mTimeTickHandler = new Handler() {
+            @Override
+            public void handleMessage(Message message) {
+                switch (message.what) {
+                    case MSG_UPDATE_TIME:
+                        invalidate();
+                        if (shouldTimerBeRunning()) {
+                            long timeMs = System.currentTimeMillis();
+                            long delayMs = TICK_PERIOD
+                                    - (timeMs % TICK_PERIOD);
+                            mTimeTickHandler.sendEmptyMessageDelayed(MSG_UPDATE_TIME, delayMs);
+                        }
+                        break;
+                }
+            }
+        };
 
         @Override
         public void onCreate(SurfaceHolder holder) {
@@ -31,11 +55,13 @@ public class CatWatchFaceService extends CanvasWatchFaceService {
         @Override
         public void onTimeTick() {
             super.onTimeTick();
+            invalidate();
         }
 
         @Override
         public void onAmbientModeChanged(boolean inAmbientMode) {
             super.onAmbientModeChanged(inAmbientMode);
+            updateTimer();
         }
 
         @Override
@@ -43,9 +69,28 @@ public class CatWatchFaceService extends CanvasWatchFaceService {
             super.onDraw(canvas, bounds);
             mWatchFace.draw(canvas, bounds);
         }
+
         @Override
         public void onVisibilityChanged(boolean visible) {
             super.onVisibilityChanged(visible);
+            updateTimer();
+        }
+
+        private void updateTimer() {
+            mTimeTickHandler.removeMessages(MSG_UPDATE_TIME);
+            if (shouldTimerBeRunning()) {
+                mTimeTickHandler.sendEmptyMessage(MSG_UPDATE_TIME);
+            }
+        }
+
+        private boolean shouldTimerBeRunning() {
+            return isVisible() && !isInAmbientMode();
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            mTimeTickHandler.removeMessages(MSG_UPDATE_TIME);
         }
     }
 }
